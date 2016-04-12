8ba1350680c03b30e865e484d20b40427734cd31
Dario Cappellini <dcappellini@hotmail.com.ar>
2016-04-11 21:34:06 -0300

    Agrego media (modelos de autos)

--- EjemploAlumno.cs
+++ EjemploAlumno.cs
@@ -17,6 +17,9 @@ namespace AlumnoEjemplos.MiGrupo
     /// </summary>
     public class EjemploAlumno : TgcExample
     {
+        TgcBox box;
+        TgcScene scene;
+        string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosDir;
 
         /// <summary>
         /// Categoría a la que pertenece el ejemplo.
@@ -50,84 +53,26 @@ namespace AlumnoEjemplos.MiGrupo
         /// </summary>
         public override void init()
         {
+            TgcSceneLoader loader = new TgcSceneLoader();
+            scene = loader.loadSceneFromFile(alumnoMediaFolder + "MiGrupo\\media\\PuebloJapones\\PuebloJapones-TgcScene.xml");
+
+
+            box = new TgcBox();
+            box.Size = new Vector3(20, 20, 20);
+            box.Position = new Vector3(0, 0, 0);
+            box.Color = Color.Crimson;
+            
             //GuiController.Instance: acceso principal a todas las herramientas del Framework
 
             //Device de DirectX para crear primitivas
             Device d3dDevice = GuiController.Instance.D3dDevice;
 
-        
+
+            GuiController.Instance.RotCamera.CameraDistance = 100;
+            
+
             //Carpeta de archivos Media del alumno
-            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
-
-
-            ///////////////USER VARS//////////////////
-
-            //Crear una UserVar
-            GuiController.Instance.UserVars.addVar("variablePrueba");
-
-            //Cargar valor en UserVar
-            GuiController.Instance.UserVars.setValue("variablePrueba", 5451);
-
-
-
-            ///////////////MODIFIERS//////////////////
-
-            //Crear un modifier para un valor FLOAT
-            GuiController.Instance.Modifiers.addFloat("valorFloat", -50f, 200f, 0f);
-
-            //Crear un modifier para un ComboBox con opciones
-            string[] opciones = new string[]{"opcion1", "opcion2", "opcion3"};
-            GuiController.Instance.Modifiers.addInterval("valorIntervalo", opciones, 0);
-
-            //Crear un modifier para modificar un vértice
-            GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
-
-
-
-            ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
-            //Es la camara que viene por default, asi que no hace falta hacerlo siempre
-            GuiController.Instance.RotCamera.Enable = true;
-            //Configurar centro al que se mira y distancia desde la que se mira
-            GuiController.Instance.RotCamera.setCamera(new Vector3(0, 0, 0), 100);
-
-
-            /*
-            ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
-            //Camara en primera persona, tipo videojuego FPS
-            //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
-            //Por default la camara FPS viene desactivada
-            GuiController.Instance.FpsCamera.Enable = true;
-            //Configurar posicion y hacia donde se mira
-            GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
-            */
-
-
-
-            ///////////////LISTAS EN C#//////////////////
-            //crear
-            List<string> lista = new List<string>();
-
-            //agregar elementos
-            lista.Add("elemento1");
-            lista.Add("elemento2");
-
-            //obtener elementos
-            string elemento1 = lista[0];
-
-            //bucle foreach
-            foreach (string elemento in lista)
-            {
-                //Loggear por consola del Framework
-                GuiController.Instance.Logger.log(elemento);
-            }
-
-            //bucle for
-            for (int i = 0; i < lista.Count; i++)
-            {
-                string element = lista[i];
-            }
-
-
+        
         }
 
 
@@ -141,24 +86,19 @@ namespace AlumnoEjemplos.MiGrupo
         {
             //Device de DirectX para renderizar
             Device d3dDevice = GuiController.Instance.D3dDevice;
-            
-
-            //Obtener valor de UserVar (hay que castear)
-            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
+            GuiController.Instance.RotCamera.targetObject(box.BoundingBox);
 
 
-            //Obtener valores de Modifiers
-            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
-            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
-            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
+            //box.setTexture(TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosDir + "\\MiGrupo\\media\\original.png"));
 
 
             ///////////////INPUT//////////////////
             //conviene deshabilitar ambas camaras para que no haya interferencia
 
             //Capturar Input teclado 
-            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            if (GuiController.Instance.D3dInput.keyDown(Microsoft.DirectX.DirectInput.Key.F))
             {
+                box.move(0.1f, 0, 0);
                 //Tecla F apretada
             }
 
@@ -168,6 +108,10 @@ namespace AlumnoEjemplos.MiGrupo
                 //Boton izq apretado
             }
 
+            box.updateValues();
+            scene.renderAll();
+            box.render();
+
         }
 
         /// <summary>
@@ -176,7 +120,7 @@ namespace AlumnoEjemplos.MiGrupo
         /// </summary>
         public override void close()
         {
-
+            box.dispose();
         }
 
     }
