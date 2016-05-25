189cc4b20c189586e301e29d255368bbca6f584d
Alexander Poschenrieder <aposchenrieder@yahoo.com.ar>
2016-05-25 12:13:52 -0300

    Corrección auto. variable rotacion

--- EjemploAlumno.cs
+++ EjemploAlumno.cs
@@ -656,7 +656,7 @@ namespace AlumnoEjemplos.MiGrupo
             TgcD3dInput input = GuiController.Instance.D3dInput;
             if (input.keyDown(Key.LeftShift))
             {
-                camaraAuto1.RotationY = mainCar.rotacionY;
+                camaraAuto1.RotationY = mainCar.rotacion;
                 camaraAuto1.Target = autoPos;
                 camaraActiva1 = camaraAuto1;
             }
@@ -670,7 +670,7 @@ namespace AlumnoEjemplos.MiGrupo
             //conviene deshabilitar ambas camaras para que no haya interferencia
             if (input.keyDown(Key.RightShift))
             {
-                camaraAuto2.RotationY = secondCar.rotacionY;
+                camaraAuto2.RotationY = secondCar.rotacion;
                 camaraAuto2.Target = auto2Pos;
                 camaraActiva2 = camaraAuto2;
             }
